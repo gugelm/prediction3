c4246b05e06647bf3a8f12a1e1085a69367ff905
gugelm@gmail.com <gugelm@gmail.com>
2020-05-31 09:46:26 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,36 +1,92 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { View, FlatList, Text, TextInput } from 'react-native';
+import {ListItem, Input, Button } from "react-native-elements";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Icon } from 'react-native-vector-icons/FontAwesome'
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+let predictionData = [
+  {
+  "key":"1",
+  "prediction":"Instagram has peaked.",
+  "created_date":"5/1/2019",
+  "prob":"90%",
+  "deadline":"5/1/2022",
+  "reasoning": "My friends have stopped using it. People are tired of sees doggos and vacation and perfectly curated shots. Young kids are purposefully making their pics look like shit to be more authentic."
+  }, 
+  {
+  "key":"2",
+  "prediction":"Trump will win the 2020 election.",
+  "created_date":"10/8/2019",
+  "prob":"80%",
+  "deadline":"11/3/2020",
+  "reasoning": "From a trump’s supporters perspective, he’s done a good job. The economy is strong, he’s avoid conflict (e.g. Iran). Dems sense weakness i  their candidates and are pushing impeachment 1 year from the election. Kelly says 50% on 11/15/2019z"
+  }]
+let predictionList = predictionData
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+let searchFilterFunction = (value) => {}
+
+function HomeScreen({ navigation }) {
+  let [ predictionData, searchFilterFunction] = React.useState(predictionList);
+  return ( 
+    <View>
+    <FlatList 
+    data={predictionData}
+    renderItem={({ item }) => (
+    <ListItem
+      key={item.key}
+      title={item.prediction}
+      bottomDivider
+      chevron
+      onPress={() =>
+        navigation.navigate(
+          'DetailsScreen', 
+          {key: [item.key], prediction: [item.prediction], deadline: [item.deadline]})
+        }
+      />
+    )} 
+    />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+function DetailsScreen({ route, navigation }) {
+  const {key} = route.params
+  const {prediction} = route.params
+  const {deadline} = route.params
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Input
+        label='Prediction'
+        value={prediction}
+        placeholder='Enter a quantifiable prediction.'
+        onChangeText={value => searchFilterFunction(value)}
+      />
+      <Input
+        label= 'Deadline'
+        value={deadline} 
+        placeholder='Enter a deadline'
+      />
+      <View style={{ flexDirection: 'row'}}>
+      <Button 
+        title='Done'
+        style={{padding: 10}} 
+      />
+      </View>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="Predictions" component={HomeScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
